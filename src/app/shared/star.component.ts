f040b0ae7d1eae11a16f1513c0b19858fcec5f08
Ifeanyi30 <liamsanctus30@gmail.com>
2022-02-15 13:08:48 +0100

    Revert "containerize the code and refactoiring"
    This reverts commit a56351d36284276512b7cac8d49562d9be871a85.

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -6,7 +6,7 @@ import { Component, OnChanges, Input, Output, EventEmitter } from "@angular/core
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    @Input() rating: number = 0;
+    @Input() rating: number = 4;
     cropWidth: number = 75;
     @Output() notify: EventEmitter<string> = new EventEmitter<string>();
 
